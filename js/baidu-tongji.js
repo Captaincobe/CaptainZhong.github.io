0c85d978d6dea9b91dcd2aaff874fd0a51d30e5e
Captaincobe <o2cator@163.com>
2024-05-31 09:45:47 +0800

    Site updated: 2024-05-31 09:45:47

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,8 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?8f51d5fc6f20da4cb708b9e81d87da10";
+  hm.src = "https://hm.baidu.com/hm.js?56c6909e83b7ac572c77b1a46618cd25";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
